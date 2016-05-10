b03d1163295fe4f934a59f8124a83569287a112d
katieO <katajello@yahoo.com>
2016-05-09 20:12:20 -0700

    Mark player squares

--- app.js
+++ app.js
@@ -23,10 +23,14 @@ for(var i = 0; i < squares.length; i = i + 1) {
   squares[i].addEventListener('click', function(){
     console.log(this)
     // check if the square has already been marked
-
+    function playerMove(square) {
+      if (square.innerHTML == "") {
+        square.innerHTML = currentPlayer.marker;
+      }
+    }
     //  mark an x or an o
     // check to see if there's a winner
-
+    playerMove(this)
     switchTurns()
   })
 }
